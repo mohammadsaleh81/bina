8e96c2f7fe67fbeadd51320140993d743a871d42
mohammad <mohammadsalehi8138@gmail.com>
2021-07-25 12:13:13 +0430

    create-model-documents

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
